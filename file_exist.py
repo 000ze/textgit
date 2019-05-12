d68bee7fdd70093457240c2e6e2975403d73745e
00ze <1413511414@qq.com>
2019-05-12 15:57:57 +0800

    file_exist.py

---
+++ file_exist.py
@@ -0,0 +1,13 @@
+import os
+if os.path.exists('split_text.txt'):
+     date=open('split_text.txt')
+     for each_line in date:
+        if each_line.find(':')!=-1:
+            (role,line_spoken)=each_line.split(':',1)  # 这是一个元组，1是指定
+            print(' ')
+            print(role,end=" ")
+            print('said:',end=" ")
+            print(line_spoken,end=" ")
+     date.close()
+else :
+    print('Sorry,this file is not exist')
